179689fd8da135b297ea655450b81c76261058ea
Asad <aanaqvi51@gmail.com>
2023-12-29 03:19:01 +0500

    Initial commit

--- sentiment-analysis-backend/index.js
+++ sentiment-analysis-backend/index.js
@@ -1,18 +1,39 @@
-// In sentiment-analysis-backend/index.js
-
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.post('/analyze', async (req, res) => {
-  const text = req.body.text;
-  // TODO: Call an external sentiment analysis API
-  // For example, using axios to send a request
-  // const result = await axios.post('external-api-url', { text });
-  // res.json({ sentiment: result.data.sentiment });
+  const { text } = req.body; // Make sure 'text' is passed from the frontend
 
-  res.json({ sentiment: 'Positive/Negative/Neutral' }); // Placeholder response
+  const options = {
+    method: 'POST',
+    url: 'https://api.edenai.run/v2/text/entity_sentiment',
+    headers: {
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYzFjNWNjYTAtMzFiZi00YjNhLWE5NGQtNjcxYTI1NzEyYzk3IiwidHlwZSI6ImFwaV90b2tlbiJ9.utEhImw2CeLGZSx1nEzqLibD-6fd09sGHn7e7YXrjAY',
+      'Content-Type': 'application/json',
+    },
+    data: JSON.stringify({
+      providers: "amazon", // Note: providers as a single string
+      text: text,
+      language: "en",
+      fallback_providers: ""
+    })
+  };
+
+  try {
+    const response = await axios.request(options);
+    res.json(response.data);
+  } catch (error) {
+    console.error('Backend Error:', error);
+    res.status(500).json({
+      message: 'Internal Server Error',
+      error: error.message,
+      details: error.response ? error.response.data : null,
+    });
+  }
 });
 
 const PORT = 5000;
